b92adf01508879460b290d5ae1fb3eb1a7991525
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-09-29 23:42:33 +0100

    Move renderer registration to relevant BlockEntities

--- src/main/java/net/devrune/messenger_crow/block/entity/ScarecrowBlockEntity.java
+++ src/main/java/net/devrune/messenger_crow/block/entity/ScarecrowBlockEntity.java
@@ -1,5 +1,12 @@
 package net.devrune.messenger_crow.block.entity;
 
+import net.devrune.messenger_crow.MessengerCrowMod;
+import net.devrune.messenger_crow.block.renderer.ScarecrowTileRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import software.bernie.geckolib.util.GeckoLibUtil;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.core.animation.RawAnimation;
@@ -196,5 +203,15 @@ public class ScarecrowBlockEntity extends RandomizableContainerBlockEntity imple
 		super.setRemoved();
 		for (LazyOptional<? extends IItemHandler> handler : handlers)
 			handler.invalidate();
+	}
+
+
+	@Mod.EventBusSubscriber(modid = MessengerCrowMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class ClientEvents {
+		@OnlyIn(Dist.CLIENT)
+		@SubscribeEvent
+		public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+			event.registerBlockEntityRenderer(MessengerCrowModBlockEntities.SCARECROW.get(), context -> new ScarecrowTileRenderer());
+		}
 	}
 }
